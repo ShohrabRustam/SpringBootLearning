113d967151a3bc729c8d620b2bb5c2151ff5ad8d
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-01 13:36:10 +0530

    import Request mapping for url

--- HelloWorld/src/main/java/com/example/demo/AppConfiguration.java
+++ HelloWorld/src/main/java/com/example/demo/AppConfiguration.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class AppConfiguration {
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "Hello World";
+    }
 
 }
