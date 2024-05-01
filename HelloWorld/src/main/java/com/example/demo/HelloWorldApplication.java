43bdba7ee18e0a8b7aac37daffdf7eaf1124c1d4
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-01 13:40:43 +0530

    remove print statement from HelloWorldApplication.java

--- HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -8,7 +8,6 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.println("Hello World!");
 	}
 
 }
