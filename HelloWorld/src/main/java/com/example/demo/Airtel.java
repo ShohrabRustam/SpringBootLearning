0edca2c65f7e15ba4b138cd7f7f3f2caa4e9cb0a
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-02 14:56:39 +0530

    create two class for jio and airtel

---
+++ HelloWorld/src/main/java/com/example/demo/Airtel.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+ // Class
+// Implementing Sim interface
+public class Airtel implements Sim {
+ 
+    @Override public void calling()
+    {
+        System.out.println("Airtel Calling");
+    }
+ 
+    @Override public void data()
+    {
+        System.out.println("Airtel Data");
+    }
+}
