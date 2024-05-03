9cdf10a6f4dd864559aa4535a3245901d00f6593
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-03 17:05:51 +0530

    calling SIM using ApplicationContext as a Part of Inversion of Controller

--- HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,18 +1,23 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-// import com.example.demo.Airtel;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-		ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
-		Sim sim = context.getBean(Jio.class);
+//		SpringApplication.run(HelloWorldApplication.class, args);
+//		ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+//		Sim sim = context.getBean(Jio.class);
+//		sim.calling();
+//		sim.data();
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+		Sim sim = applicationContext.getBean("sim", Sim.class);
 		sim.calling();
 		sim.data();
+
 	}
 
 }
