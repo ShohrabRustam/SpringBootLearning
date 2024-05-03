bb8c5c649c8e8424c3319fcfbe9192e9b128ec11
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-03 15:17:47 +0530

    ConfigurableApplicationContext to get the object

--- HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ConfigurableApplicationContext;
+// import com.example.demo.Airtel;
 @SpringBootApplication
 public class HelloWorldApplication {
 
