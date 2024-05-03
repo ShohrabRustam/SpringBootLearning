0745a13f44733a43d9f9b849523c4d5fe57a93fa
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-03 10:21:09 +0530

    get class object by getBean

--- HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -8,6 +8,14 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+		Sim sim = context.getBean(Jio.class);
+
+//		 Sim sim = new Jio();
+//         Sim sim = new Airtel();
+         sim.calling();
+//         sim.data();
+		System.out.println("Running: ");
 	}
 
 }
