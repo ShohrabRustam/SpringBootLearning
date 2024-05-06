1215bd2b31b2565c9866184d6bcfbc3ca92afbd8
shorab.rustam <shorab.rustam@mpokket.com>
2024-05-06 13:45:00 +0530

    Dependency Injection calling using beans

--- DependencyInjection/src/main/java/com/example/demo/DependencyInjectionApplication.java
+++ DependencyInjection/src/main/java/com/example/demo/DependencyInjectionApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+//		SpringApplication.run(DependencyInjectionApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class,args);
+		Customers c = context.getBean(Customers.class);
+		c.display();
 	}
 
 }
